bc939ca2e9fc7a0c34c64d91d78450a6cdbc9c04
azahel79 <azahelprogramer79@gmail.com>
2022-07-22 23:41:43 -0500

    actualizacion

--- db/coneccionDB.js
+++ db/coneccionDB.js
@@ -5,7 +5,8 @@ const coneccionDB = new Sequelize({
      username: "root",
      password: "root",
      dialect: "mysql",
-     host: "localhost",
+     host: "127.0.0.1",
+     port: 3306,
      database: "shop_appi"
 })
 
